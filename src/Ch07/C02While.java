8517034593e9c1421cc95e91a111fede935aa822
HongJeHyeop <jhh951217@naver.com>
2023-01-10 18:34:38 +0900

    Day10

--- src/Ch07/C02While.java
+++ src/Ch07/C02While.java
@@ -182,37 +182,94 @@ public class C02While {
 //		}
 		
 		// 높이 입력해서 받는 작업
-		Scanner sc = new Scanner(System.in);
-		int i = 0; // 개행
-		int j = 0; // 공백
-		int s = 0; // 별
-		int h = sc.nextInt(); // 높이
-		
-		while(i < h) {
-			j = 0;
-			while(j <= (h-2)-i) {
-				System.out.print(" ");
-				j++;
-			}
-			s = 0;
-			while(s <= i*2) {
-				System.out.print("*");
-				s++;
-			}
-			System.out.println();
-			i++;
-		}
-					
-		
+//		Scanner sc = new Scanner(System.in);
+//		int i = 0; // 개행
+//		int j = 0; // 공백
+//		int s = 0; // 별
+//		int h = sc.nextInt(); // 높이
+//		
+//		while(i < h) {
+//			j = 0;
+//			while(j <= (h-2)-i) {
+//				System.out.print(" ");
+//				j++;
+//			}
+//			s = 0;
+//			while(s <= i*2) {
+//				System.out.print("*");
+//				s++;
+//			}
+//			System.out.println();
+//			i++;
+//		}
+//		[규칙]
+//		i(개행)		j(공백)		s(별)
+//		0			0-(h-2)-0	0-0
+//		1			0-(h-2)-1	0-2
+//		2			0-(h-2)-2	0-4
+//		3			0-(h-2)-3	0-6
+//		---------------------------------
+//		i = 0;	    j = 0;		s = 0;
+//		i < h	  j <= (h-2)-i  s <= i*2
+//		i++;		j++;		s++;
+//		---------------------------------
 		
 		// 5
 		// *******
 		//  *****
 		//   ***
 		//    *
+//		int i = 0; // 개행
+//		int j = 0; // 공백
+//		int s = 0; // 별
+//		
+//		while(i < 4) {
+//			j = 0;
+//			while(j<= i-1) {
+//				System.out.print(" ");
+//				j++;
+//			}
+//			s = 0;
+//			while(s <= (3 - i) * 2 ) {
+//				System.out.print("*");
+//				s++;
+//			}
+//			System.out.println();
+//			i++;
+//		}
+//		i(개행)		j(공백)		s(별)
+//		0			x			0-6
+//		1			0-1			0-4
+//		2			0-2			0-2
+//		3			0-3			0-0
+//		---------------------------------
+//		i = 0;	    j = 0;		s = 0;
+//		j<= i-1	   j<= i-1  s <= (3 - i) * 2
+//		i++;		j++;		s++;
+//		---------------------------------
 		
 		
+		// 입력받은 역삼각형
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int i = 0; // 개행
+		int j = 0; // 공백
+		int s = 0; // 별
 		
+		while(i < h) {
+			j = 0;
+			while(j<= i-1) {
+				System.out.print(" ");
+				j++;
+			}
+			s = 0;
+			while(s <= ((h-1) - i) * 2 ) {
+				System.out.print("*");
+				s++;
+			}
+			System.out.println();
+			i++;
+		}
 		
 		
 		
---
+++ src/Ch07/C04For.java
@@ -0,0 +1,32 @@
+package Ch07;
+
+public class C04For {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("HELLO WORLD");
+		}
+
+		/** [문제] **/
+		// 1. 1부터 10까지의 합
+		// 2. 1부터 N까지의 합
+		// 3. N부터 N까지의 합
+		// 4. 구구단 2단 출력
+		// 5. 구구단 N단 출력
+		// 6. 구구단 2-9단 출력
+		// 7. 구구단 2-N단 출력
+		// 8. 구구단 N-N단 출력
+		// 9. 별찍기
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
